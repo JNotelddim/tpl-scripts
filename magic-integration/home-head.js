0b39c3c1f8548a0f7b7c40dbb704a13e5824f10f
JNotelddim <JNotelddim@users.noreply.github.com>
2022-05-09 12:31:17 -0700

    Scrap code for hiding/showing elements
    just let the auth code decide which pages are acceptable

--- magic-integration/home-head.js
+++
@@ -1,8 +0,0 @@
-function tryGetUser() {
-  if (typeof getUser === "function") {
-    getUser();
-  } else {
-    console.error("getUser fn not present", getUser);
-  }
-}
-tryGetUser();
--- magic-integration/sitewide-head.js
+++
@@ -1,2 +0,0 @@
-/** Initialize Magic */
-const magic = new Magic("pk_live_9A73296BE1EA06BB");
